3c027b9bed6360a742ad318bca08ff338148d6e2
Daniel Lamando <dan@danopia.net>
2021-01-22 14:07:41 +0100

    Restructure & add Datadog compatibility

---
+++ lib/specification.ts
@@ -0,0 +1,47 @@
+// https://github.com/OpenObservability/OpenMetrics/blob/master/specification/OpenMetrics.md
+
+export type ExpositionFormat = 'openmetrics' | 'legacy';
+export const ContentTypes = {
+  openmetrics: 'application/openmetrics-text; version=1.0.0; charset=utf-8',
+  legacy: 'text/plain; version=0.0.4',
+};
+
+export function buildMetricsPayload(source: Generator<OpenMetric>, fmt: ExpositionFormat) {
+  const accum = new Array<string>();
+  for (let {prefix, type, unit, help, values} of source) {
+
+    // Downgrade counters for original prometheus requests
+    if (fmt === 'legacy' && type === 'counter') {
+      // the _total part moves up a level and non-_total metrics are stripped
+      prefix += '_total';
+      values = new Map(Array.from(values)
+        .filter(x => x[0].startsWith(prefix)));
+    }
+
+    accum.push(`# TYPE ${prefix} ${type}\n`);
+    if (unit && fmt === 'openmetrics') accum.push(`# UNIT ${prefix} ${unit}\n`);
+    if (help) accum.push(`# HELP ${prefix} ${help}\n`);
+    for (const point of values) {
+      accum.push(`${prefix}${point[0]} ${point[1]}\n`)
+    }
+  }
+  if (fmt === 'openmetrics') accum.push(`# EOF\n`);
+  return accum.join('');
+}
+
+export function buildMetricsResponse(source: Generator<OpenMetric>, fmt: ExpositionFormat) {
+  return {
+    body: buildMetricsPayload(source, fmt),
+    headers: new Headers({
+      'content-type': ContentTypes[fmt],
+    }),
+  }
+}
+
+export interface OpenMetric {
+  prefix: string;
+  type: string;
+  unit?: string;
+  help?: string;
+  values: Map<string, number | string>;
+}
--- mod.ts
+++ mod.ts
@@ -1,18 +1,36 @@
-import { serve } from "https://deno.land/std@0.84.0/http/server.ts";
+import { serve, ServerRequest } from "https://deno.land/std@0.84.0/http/server.ts";
 
-import { buildMetricsResponse } from './metrics/specification.ts';
-import { gatherEverything } from './metrics/deno-metrics.ts';
+import { buildMetricsResponse, OpenMetric } from './lib/specification.ts';
+import { gatherEverything } from './lib/deno-metrics.ts';
 
-export async function runMetricsServer(opts?: Pick<Deno.ListenOptions, "port" | "hostname">) {
+export * from './lib/specification.ts';
+export * from './lib/deno-metrics.ts';
+
+export function runMetricsServer(opts?: Pick<Deno.ListenOptions, "port" | "hostname">) {
   const server = serve(opts ?? { port: 9090 });
 
-  for await (const req of server) {
+  // go work the server on its own
+  (async function() {
+    for await (const req of server) {
 
-    if (req.url === '/metrics') {
-      req.respond(buildMetricsResponse(gatherEverything()));
-      continue;
+      if (req.url === '/metrics') {
+        respondToScrape(req, gatherEverything());
+        continue;
+      }
+
+      req.respond({ status: 404, body: "Not Found\n" });
     }
+  }());
 
-    req.respond({ status: 404, body: "Not Found\n" });
+  return server;
+}
+
+export function respondToScrape(req: ServerRequest, stream: Generator<OpenMetric>) {
+  // datadog only acceppts prometheus payloads
+  if (req.headers.get('user-agent')?.startsWith('Datadog Agent/')) {
+    return req.respond(buildMetricsResponse(stream, 'legacy'));
   }
+
+  // prometheus since 2.5.0 (~2018) has supported OpenMetrics
+  req.respond(buildMetricsResponse(stream, 'openmetrics'));
 }

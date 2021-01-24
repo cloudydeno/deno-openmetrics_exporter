27cf6a4e39cc2f9aef3b2e8e8379f56b8b96821e
Daniel Lamando <dan@danopia.net>
2021-01-24 15:57:11 +0100

    Update test suite, add procfs test

--- mod_test.ts
+++ mod_test.ts
@@ -1,13 +1,17 @@
-import { runMetricsServer, MetricsRegistry, scrapeDenoMetrics } from './mod.ts';
 import { assertEquals, assertArrayIncludes } from "https://deno.land/std@0.84.0/testing/asserts.ts";
 
-function setupMetricServer() {
+import { runMetricsServer, MetricsRegistry } from './mod.ts';
+import { scrapeDenoMetrics } from './lib/instrumented/deno.ts';
+
+function setupMetricServer(sources: Array<'deno' | 'denohttp'> = ['deno', 'denohttp']) {
   const registry = new MetricsRegistry();
-  registry.sources.push({
+  if (sources.includes('deno')) registry.sources.push({
     scrapeMetrics: scrapeDenoMetrics,
   });
 
-  const server = runMetricsServer({ port: 0, hostname: 'localhost' }, registry);
+  const server = runMetricsServer({ port: 0, hostname: 'localhost', registry });
+  if (sources.includes('denohttp')) registry.sources.push(server);
+
   const {port} = server.listener.addr as Deno.NetAddr;
   const url = `http://localhost:${port}`;
   return {registry, server, url};
@@ -25,6 +29,7 @@ Deno.test("basics", async () => {
     '# UNIT deno_ops_sent_bytes bytes',
     'deno_open_resources{res_type="stdin"} 1',
     'deno_open_resources{res_type="tcpListener"} 1',
+    'denohttp_requests_in_flight 1',
     '# EOF',
   ]);
 
@@ -38,6 +43,7 @@ Deno.test("datadog compatibility", async () => {
     headers: {
       'User-Agent': 'Datadog Agent/0.0.0',
     }});
+    assertEquals(resp.status, 200);
   const lines = await resp.text().then(x => x.split('\n'));
   assertArrayIncludes(lines, [
     'deno_ops_completed_total{op_type="async_unref"} 0',
@@ -47,8 +53,27 @@ Deno.test("datadog compatibility", async () => {
   server.close();
 });
 
+Deno.test("browser compatibility", async () => {
+  const {server, url} = setupMetricServer(['deno']);
+
+  const resp = await fetch(`${url}/metrics`, {
+    headers: {
+      'User-Agent': "Mozilla/5.0 (X11; CrOS x86_64 13505.73.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.109 Safari/537.36",
+    }});
+  assertEquals(resp.status, 200);
+  assertEquals(resp.headers.get('content-type'), 'text/plain');
+  const lines = await resp.text().then(x => x.split('\n'));
+  assertArrayIncludes(lines, [
+    'deno_ops_completed_total{op_type="async_unref"} 0',
+    '# TYPE deno_ops_sent_bytes counter', // openmetrics, not prometheus
+    '# EOF',
+  ]);
+
+  server.close();
+});
+
 Deno.test("no root route", async () => {
-  const {server, url} = setupMetricServer();
+  const {server, url} = setupMetricServer([]);
 
   const resp = await fetch(`${url}/not-found`);
   assertEquals(resp.status, 404);

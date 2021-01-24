84cc967c7e9a3ed85f401435d3747fd221c9c233
Daniel Lamando <dan@danopia.net>
2021-01-24 17:37:58 +0100

    Add a bit of fetch() instrumentation, add to demo

--- demo.ts
+++ demo.ts
@@ -1,9 +1,14 @@
 import { runMetricsServer } from './mod.ts';
+import { fetch } from './lib/instrumented/fetch.ts';
 
+// Start a web server for metrics
 runMetricsServer({ port: 9090 });
 console.log('Now serving http://localhost:9090/metrics');
 
 // Hit ourselves to simulate continuous traffic over time
 setInterval(() => {
   fetch('http://localhost:9090').then(x => x.text());
-}, 1000);
+}, 2500);
+
+// Make a dummy bad request
+fetch('http://asdfffffff.com').catch(err => {})

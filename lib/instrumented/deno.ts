9425a42bd92595e75fb45034df70d8eff61aa10d
Daniel Lamando <dan@danopia.net>
2021-01-29 07:00:30 +0100

    deno: Add experimental per-op metric support
    Currently only works when running https://github.com/denoland/deno/pull/9240
    with the --unstable flag. Otherwise reports as before.

--- lib/instrumented/deno.ts
+++ lib/instrumented/deno.ts
@@ -14,44 +14,58 @@ DefaultRegistry.sources.push({
 });
 
 export function* buildDenoOpsMetrics(metrics: Deno.Metrics): Generator<OpenMetric> {
+  // API which might be exposed if Deno is running w/ --unstable
+  // Feel for it instead of actually needing --unstable types to compile
+  // https://github.com/denoland/deno/pull/9240/files
+  const perOps = (metrics as any).ops as Record<string,Deno.Metrics> | undefined;
+  if (perOps) {
+    // Clean op names a little bit
+    yield* buildDenoPerOpMetrics(Object
+      .entries(perOps)
+      .map(([opId, metrics]) => {
+        const opName = opId.replace(/^op_/, '').replace(/_a?sync$/, '');
+        return [`,deno_op=${JSON.stringify(opName)}`, metrics];
+      }));
+
+  } else {
+    // If per-op API isn't available then just don't attach a tag
+    yield* buildDenoPerOpMetrics([['', metrics]]);
+  }
+}
+
+export function* buildDenoPerOpMetrics(ops: Array<[string,Deno.Metrics]>): Generator<OpenMetric> {
   yield {
     prefix: 'deno_ops_dispatched',
     type: 'counter',
-    values: new Map([
-      ['_total{op_type="sync"}', metrics.opsDispatchedSync],
-      ['_total{op_type="async"}', metrics.opsDispatchedAsync],
-      ['_total{op_type="async_unref"}', metrics.opsDispatchedAsyncUnref],
-    ])};
+    values: new Map(ops.flatMap(([opFacet, metrics]): [string, number][] => [
+      [`_total{op_type="sync"${opFacet}}`, metrics.opsDispatchedSync],
+      [`_total{op_type="async"${opFacet}}`, metrics.opsDispatchedAsync],
+      [`_total{op_type="async_unref"}${opFacet}`, metrics.opsDispatchedAsyncUnref],
+    ]).filter(x => x[1] > 0))};
   yield {
     prefix: 'deno_ops_completed',
     type: 'counter',
-    values: new Map([
-      ['_total{op_type="sync"}', metrics.opsCompletedSync],
-      ['_total{op_type="async"}', metrics.opsCompletedAsync],
-      ['_total{op_type="async_unref"}', metrics.opsCompletedAsyncUnref],
-    ])};
+    values: new Map(ops.flatMap(([opFacet, metrics]): [string, number][] => [
+      [`_total{op_type="sync"${opFacet}}`, metrics.opsCompletedSync],
+      [`_total{op_type="async"${opFacet}}`, metrics.opsCompletedAsync],
+      [`_total{op_type="async_unref"${opFacet}}`, metrics.opsCompletedAsyncUnref],
+    ]).filter(x => x[1] > 0))};
 
   yield {
     prefix: 'deno_ops_sent_bytes',
     type: 'counter',
     unit: 'bytes',
-    values: new Map([
-      ['_total', metrics.bytesSentData],
-    ])};
-  yield {
-    prefix: 'deno_ops_sent_control_bytes',
-    type: 'counter',
-    unit: 'bytes',
-    values: new Map([
-      ['_total', metrics.bytesSentControl], // TODO: Seems like this is a subset of 'sent data' but currently unclear
-    ])};
+    values: new Map(ops.flatMap(([opFacet, metrics]): [string, number][] => [
+      [`_total{send_slot="control"${opFacet}}`, metrics.bytesSentControl],
+      [`_total{send_slot="data"${opFacet}}`, metrics.bytesSentData],
+    ]).filter(x => x[1] > 0))};
   yield {
     prefix: 'deno_ops_received_bytes',
     type: 'counter',
     unit: 'bytes',
-    values: new Map([
-      ['_total', metrics.bytesReceived],
-    ])};
+    values: new Map(ops.flatMap(([opFacet, metrics]): [string, number][] => [
+      [`_total{recv_slot="response"${opFacet}}`, metrics.bytesReceived],
+    ]))};
 }
 
 export function* buildDenoResMetrics(resources: Deno.ResourceMap): Generator<OpenMetric> {
